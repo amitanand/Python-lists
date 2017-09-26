80da3ffde6babdc53f189bd869585c9819cc300f
Amit Anand <anand.amit943@gmail.com>
2017-09-26 09:39:07 -0700

    to check whether the list is empty or not

---
+++ checkemptylist.py
@@ -0,0 +1,11 @@
+# Write a Python program to check a list is empty or not ?
+
+def empty_check(list):
+    if not list:
+        print("list is empty !")
+
+    else:
+        print("List is not empty !")
+
+empty_check([])
+empty_check([1,5])
