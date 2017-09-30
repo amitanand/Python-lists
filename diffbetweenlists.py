198aca7d0e31e5a4a1510778fca3e1c65eee4b04
Amit Anand <anand.amit943@gmail.com>
2017-09-29 23:54:09 -0700

    to get the difference between the 2 lists

---
+++ diffbetweenlists.py
@@ -0,0 +1,11 @@
+# Write a Python program to get the difference between the two lists ?
+
+list_1=[1,2,3,4]
+list_2=[7,8,5,2]
+set1 =set(list_1)
+set2=set(list_2)
+diff = set1 -set2
+print("Difference :",diff)
+
+
+
